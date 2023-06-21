d5d9e337cb714e40e09a07dfb27bffe7f7b8523b
guozeng <guozeng@xiaogj.com>
2023-06-21 18:12:44 +0800

    change listen ip

--- user-login/server/app.ts
+++ user-login/server/app.ts
@@ -4,9 +4,9 @@ const app = Express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.end('hello world')
+  res.end('hello world11')
 })
 
-app.listen(port, () => {
+app.listen(port, '172.18.3.163', () => {
   console.log(`Server running at ${port}`)
 })
