40ee3d599be4ea4517759d257f7015745d805e38
guozeng <guozeng@xiaogj.com>
2023-06-21 18:08:07 +0800

    add express

--- user-login/server/app.ts
+++ user-login/server/app.ts
@@ -1,1 +1,12 @@
-console.log('this is a api server')
+import Express from 'express'
+
+const app = Express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.end('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`)
+})
