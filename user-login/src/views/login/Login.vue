b4453516f63c827849ad352ed29e9e947b0af792
guozeng <guozeng@xiaogj.com>
2023-06-26 15:43:22 +0800

    自动导入

--- user-login/src/views/login/Login.vue
+++ user-login/src/views/login/Login.vue
@@ -1,1 +1,4 @@
-<template>Login</template>
+<template>
+  <el-button>dsfsdfs</el-button>
+</template>
+<script setup lang="ts"></script>
--- user-login/vite.config.ts
+++ user-login/vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from 'vite-plugin-mkcert'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mkcert()],
+  plugins: [
+    vue(),
+    mkcert(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      resolvers: [ElementPlusResolver()],
+      dirs: ['./utils/**'],
+      vueTemplate: true,
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   server: {
     https: true,
   },
