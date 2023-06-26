efc233466155fe9a9d9160f7b3e1c78d982a0454
guozeng <guozeng@xiaogj.com>
2023-06-26 11:56:39 +0800

    remove express

--- user-login/server/app.ts
+++ user-login/server/app.ts
@@ -1,12 +1,1 @@
-import Express from 'express'
-
-const app = Express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.end('hello world11')
-})
-
-app.listen(port, '172.18.3.163', () => {
-  console.log(`Server running at ${port}`)
-})
+console.log('server')
