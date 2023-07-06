710b5388aa72befbc4dd36ee52f99b2f0a499104
guozeng <guozeng@xiaogj.com>
2023-07-06 17:23:09 +0800

    1

--- user-login/src/views/home/Home.vue
+++ user-login/src/views/home/Home.vue
@@ -3,14 +3,17 @@
   <wel-table></wel-table>
 </template>
 <script setup lang="ts">
-// import { useMyFetch } from '@u/index'
-// import { ElMessage } from 'element-plus'
-// import 'element-plus/theme-chalk/el-message.css'
+import { useMyFetch } from '@u/index'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
 
-// const { error, data } = await useMyFetch('common/isLogin').get().json()
-// if (error.value) {
-//   ElMessage.error('请求错误')
-// } else {
-//   console.log(data.value)
-// }
+async function req() {
+  const { error, data } = await useMyFetch(`users/getDetailByAccount`).get().json()
+  if (error.value) {
+    ElMessage.error('请求错误')
+  } else {
+    console.log(data.value)
+  }
+}
+req()
 </script>
