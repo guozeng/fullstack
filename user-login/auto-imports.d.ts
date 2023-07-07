e2137cf382cc2bff2b316db5030a6d89a5634fc3
guozeng <guozeng@xiaogj.com>
2023-07-07 18:39:43 +0800

    ajax封装

--- user-login/auto-imports.d.ts
+++ user-login/auto-imports.d.ts
@@ -6,6 +6,7 @@ export {}
 declare global {
   const EffectScope: typeof import('vue')['EffectScope']
   const ElMessage: typeof import('./src/utils/el-msg')['ElMessage']
+  const ajax: typeof import('./src/utils/fetch')['ajax']
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed']
   const autoResetRef: typeof import('@vueuse/core')['autoResetRef']
   const computed: typeof import('vue')['computed']
@@ -293,6 +294,7 @@ declare module 'vue' {
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly ElMessage: UnwrapRef<typeof import('./src/utils/el-msg')['ElMessage']>
+    readonly ajax: UnwrapRef<typeof import('./src/utils/fetch')['ajax']>
     readonly asyncComputed: UnwrapRef<typeof import('@vueuse/core')['asyncComputed']>
     readonly autoResetRef: UnwrapRef<typeof import('@vueuse/core')['autoResetRef']>
     readonly computed: UnwrapRef<typeof import('vue')['computed']>
@@ -574,6 +576,7 @@ declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly ElMessage: UnwrapRef<typeof import('./src/utils/el-msg')['ElMessage']>
+    readonly ajax: UnwrapRef<typeof import('./src/utils/fetch')['ajax']>
     readonly asyncComputed: UnwrapRef<typeof import('@vueuse/core')['asyncComputed']>
     readonly autoResetRef: UnwrapRef<typeof import('@vueuse/core')['autoResetRef']>
     readonly computed: UnwrapRef<typeof import('vue')['computed']>
--- user-login/src/utils/fetch.ts
+++ user-login/src/utils/fetch.ts
@@ -1,4 +1,5 @@
 import { createFetch } from '@vueuse/core'
+import { stringify } from 'qs'
 const useMyFetch = createFetch({
   baseUrl: 'test/api',
   combination: 'overwrite',
@@ -11,5 +12,33 @@ const useMyFetch = createFetch({
     },
   },
 })
+enum MethodTypes {
+  GET = 'get',
+  POST = 'post',
+  DELETE = 'delete',
+  PUT = 'put',
+}
+interface IAjaxConfig {
+  data?: Record<string, any>
+  method: MethodTypes
+}
+async function ajax(
+  url: string,
+  config: IAjaxConfig = {
+    method: MethodTypes.GET,
+  }
+) {
+  if (config.data && config.method !== MethodTypes.POST) {
+    url = `${url}&${stringify(config.data)}`
+  }
+  const { isFetching, error, data } = await useMyFetch(url)[config.method]().json()
+  if (error.value) {
+    ElMessage.error('请求错误')
+  }
+  return {
+    isFetching,
+    data,
+  }
+}
 
-export { useMyFetch }
+export { useMyFetch, ajax }
